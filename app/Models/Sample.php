fd460bdc83bf9f14818e16067bf9c4aaf7a0792b
XirTech <john.darlucio022@gmail.com>
2024-11-19 08:37:35 +0800

    CRUD

--- app/Models/Sample.php
+++ app/Models/Sample.php
@@ -4,16 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Sample extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'first_name',
         'last_name'
     ];
 
-    
+
 
     protected $casts = [
         'created_at' => 'datetime',
